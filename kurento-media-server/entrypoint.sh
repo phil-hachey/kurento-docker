5020222b2d5621c0acd2d05227440515b2283875
Phil Hachey <phil.hachey@bluespurs.com>
2016-01-07 23:46:16 -0400

    Modified entrypoint.sh to allow turn and stun server configuration

--- kurento-media-server/entrypoint.sh
+++ kurento-media-server/entrypoint.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 set -e
 
-if [ ! -z "$COTURN_PORT_3478_TCP_ADDR" ]; then
-  if [ ! -z "$COTURN_PORT_3478_TCP_PORT" ]; then
+if [ ! -z "$STUN_TCP_ADDR" ]; then
+  if [ ! -z "$STUN_TCP_PORT" ]; then
     # Generate WebRtcEndpoint configuration
-    echo "stunServerAddress=$COTURN_PORT_3478_TCP_ADDR" > /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
-    echo "stunServerPort=$COTURN_PORT_3478_TCP_PORT" >> /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
+    echo "stunServerAddress=$STUN_TCP_ADDR" > /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
+    echo "stunServerPort=$STUN_TCP_PORT" >> /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
+  fi
+fi
+
+if [ ! -z "$TURN_TCP_ADDR" ]; then
+  if [ ! -z "$TURN_TCP_PORT" ]; then
+    # Generate WebRtcEndpoint configuration
+    echo "turnURL=kurento:kurento@$TURN_TCP_ADDR:$TURN_TCP_PORT" >> /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
   fi
 fi
 
